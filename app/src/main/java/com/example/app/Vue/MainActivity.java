d2cdb6d28f98000af9ef24ae7893947049624b8d
Risan Kamalakaran <rinega04@gmail.com>
2019-04-02 10:19:28 +0100

    Ajout d'une musique de fond

--- app/src/main/java/com/example/app/Vue/MainActivity.java
+++ app/src/main/java/com/example/app/Vue/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.app.Vue;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 import com.example.app.R;
 
 public class MainActivity extends AppCompatActivity{
 
-
+    MediaPlayer mediaPlayer = null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (mediaPlayer == null) {
+         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.back_sound);
+         mediaPlayer.setLooping(true);
+         mediaPlayer.start();
+        }
 
         Button button1 = (Button) findViewById(R.id.button);
 
